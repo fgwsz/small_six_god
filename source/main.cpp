05917ce3ac9603a43eb0edc16a294d8fdd444e26
fgwsz <3191000458@qq.com>
2024-11-21 12:03:57 +0800

    add god_sentences

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,5 @@
+#include<cstddef>//::size_t
+#include<cstdint>//::uint32_t
 #include<ctime>//::std::time_t,::std::strftime,::std::localtime
 #include<cstdio>//::std::printf
 #include<string>//::std::string,::std::getline,::std::to_string
@@ -5,8 +7,10 @@
 #include<random>//::std::random_device,::std::mt19937,
                 //::std::uniform_int_distribution
 #include<limits>//::std::numeric_limits
-#include<iostream>//::std::cin
+#include<iostream>//::std::cin,::std::cout
+#include<utility>//forward
 #include"result_info.hpp"//::result_info
+#include"chinese_characters.h"//::chinese_characters
 
 ::std::string time_string(void)noexcept{
     ::std::time_t time=::std::chrono::system_clock::to_time_t(
@@ -84,7 +88,67 @@ void small_six_god(void)noexcept{
         ,::std::string{::result_info[result]}.c_str()
     );
 }
+static void utf32_to_utf8(uint32_t utf32,char*utf8)noexcept{
+    if(utf32<=0x7F){
+        //U+0000 to U+007F
+        *utf8=(char)utf32;
+        *++utf8='\0';
+    }else if(utf32<=0x7FF){
+        //U+0080 to U+07FF
+        *utf8=0xC0|(utf32>>6);
+        *++utf8=0x80|(utf32&0x3F);
+        *++utf8='\0';
+    }else if(utf32<=0xFFFF){
+        //U+0800 to U+FFFF
+        *utf8=0xE0|(utf32>>12);
+        *++utf8=0x80|((utf32>>6)&0x3F);
+        *++utf8=0x80|(utf32&0x3F);
+        *++utf8='\0';
+    }else if(utf32<=0x10FFFF){
+        //U+10000 to U+10FFFF
+        *utf8=0xF0|(utf32>>18);
+        *++utf8=0x80|((utf32>>12)&0x3F);
+        *++utf8=0x80|((utf32>>6)&0x3F);
+        *++utf8=0x80|(utf32&0x3F);
+        *++utf8='\0';
+    }
+}
+template<typename _Type,size_t _n>
+static consteval size_t array_size(_Type(&array_ref)[_n])noexcept{
+    return _n;
+}
+//[begin,end)
+template<typename _Type>
+static _Type random_value(_Type&& begin,_Type&& end)noexcept{
+    ::std::random_device rd;//创建一个随机设备对象
+    ::std::mt19937 gen(rd());//使用随机设备对象作为随机数生成器的种子
+    ::std::uniform_int_distribution<_Type> dist(
+        ::std::forward<_Type>(begin),
+        ::std::forward<_Type>(end)
+    );//设置随机数的范围
+    return dist(gen);//生成随机数
+}
+static uint32_t random_chinese_character()noexcept{
+    return ::chinese_characters[
+        ::random_value<size_t>(0,::array_size(chinese_characters))
+    ];
+}
+static void god_sentences()noexcept{
+    uint32_t utf32=0;
+    char utf8[5];
+    ::std::cout<<"神谕:\n";
+    for(size_t line=0;line<4;++line){
+        ::std::cout<<"    ";
+        for(size_t count=0;count<5;++count){
+            utf32=random_chinese_character();
+            ::utf32_to_utf8(utf32,utf8);
+            ::std::cout<<utf8;
+        }
+        ::std::cout<<((line+1)%2!=0?',':'.')<<'\n';
+    }
+}
 int main(void)noexcept{
     ::small_six_god();
+    ::god_sentences();
     return 0;
 }
